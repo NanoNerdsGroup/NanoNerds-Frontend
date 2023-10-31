fcbd0360f2c96cdb0f68de950bba43072841e2b3
orlandorocahuapaya <rocahuapaya@hotmail.com>
2023-10-30 21:06:55 -0500

    feat: toolbar-top component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,22 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {HttpClientModule} from "@angular/common/http";
 import {AppRoutingModule} from "./app-routing.module";
+import { ToolbarTopComponent } from './public/toolbar-top/toolbar-top.component';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatIconModule} from "@angular/material/icon";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarTopComponent
   ],
   imports: [
     HttpClientModule,
     AppRoutingModule,
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/public/toolbar-top/toolbar-top.component.ts
@@ -0,0 +1,53 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-toolbar-top',
+  templateUrl: './toolbar-top.component.html',
+  styleUrls: ['./toolbar-top.component.css']
+})
+export class ToolbarTopComponent {
+
+  showMenu: boolean = false;
+  selectedRole: string = '';
+  toggleSideNav: boolean = true;
+  @Output() useToggleSideNav = new EventEmitter<boolean>();
+
+  constructor(private router: Router) {}
+
+  emitToggleSideNav(){
+    this.toggleSideNav = !this.toggleSideNav;
+    this.useToggleSideNav.emit(this.toggleSideNav);
+  }
+
+
+  toggleMenu() {
+    this.showMenu = !this.showMenu;
+    if (this.showMenu) {
+      document.addEventListener('click', this.hideMenuOnClick);
+    } else {
+      document.removeEventListener('click', this.hideMenuOnClick);
+    }
+  }
+
+  hideMenu() {
+    this.showMenu = false;
+  }
+
+  hideMenuOnClick(event: any) {
+    if (!event.target.closest('.profile') && this.showMenu) {
+      this.hideMenu();
+    }
+  }
+
+  selectRole(role: string) {
+    this.selectedRole = role;
+    this.hideMenu();
+
+    if (role === 'Seller') {
+      this.router.navigate(['toolbar-left-seller']);
+    } else if (role === 'Customer') {
+      this.router.navigate(['home']);
+    }
+  }
+
+}
